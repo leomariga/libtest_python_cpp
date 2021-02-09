d395ead9f13c7f24b0af801b7b869ab634801acf
Leonardo Mariga <leomariga@gmail.com>
2021-02-09 20:20:39 -0300

    add cython dependency

--- pycpp_lib/myLib.py
+++ pycpp_lib/myLib.py
@@ -5,9 +5,10 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 # mylib_core is the .so or .dll as a cross-platform name
-handle = ctypes.CDLL(dir_path + "/mylib_core")     
+#handle = ctypes.CDLL(dir_path + "/mylib_core")     
 
-handle.My_Function.argtypes = [ctypes.c_int] 
+#handle.My_Function.argtypes = [ctypes.c_int] 
   
 def My_Function(num):
-    return handle.My_Function(num)   
+    #return handle.My_Function(num)   
+    return 2
